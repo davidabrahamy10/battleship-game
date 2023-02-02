77e05b3e60e7f4080212461b3ca7efc36f5df939
David Abraham <davidabrahamy10@gmail.com>
2023-02-02 11:18:43 +0000

    Add method to print board

--- run.py
+++ run.py
@@ -15,3 +15,13 @@ class BattleshipBoard:
         A method that returns a dictionary mapping letters to numbers for use in printing the board.
         """
         return {"A": 0, "B": 1, "C": 2, "D": 3, "E": 4, "F": 5}
+
+    def print_board(self):
+        """
+        A method that prints the board.
+        """
+        print("  A B C D E F")
+        row_number = 1
+        for row in self.board:
+            print("%d|%s|" % (row_number, "|".join(row)))
+            row_number += 1
