67dfed72ca06518b1b8087e9c94907954c6da6a4
David Abraham <davidabrahamy10@gmail.com>
2023-02-02 11:20:50 +0000

    Add class and method to place ships

--- run.py
+++ run.py
@@ -25,3 +25,26 @@ class BattleshipBoard:
         for row in self.board:
             print("%d|%s|" % (row_number, "|".join(row)))
             row_number += 1
+
+class Battleship:
+    """
+    A class that represents the game Battleship.
+    """
+
+    def __init__(self, board):
+        self.board = board
+
+    def create_ships(self):
+        """
+        A method that places the ships randomly on the board.
+        """
+        for i in range(5):
+            self.number_row, self.letter_column = randint(0, 5), randint(0, 5)
+            while self.board[self.number_row][self.letter_column] == "X":
+                self.number_row, self.letter_column = randint(
+                    0, 5), randint(0, 5)
+                if all(self.board[x][y] != "X" for x in range(5) for y in range(5)):
+                    break
+            self.board[self.number_row][self.letter_column] = "X"
+
+        return self.board
