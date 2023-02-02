576c6ba5df568432571d0a96e82c6b4c9eafcb3f
David Abraham <davidabrahamy10@gmail.com>
2023-02-02 11:16:36 +0000

    Add class for battleship board

--- run.py
+++ run.py
@@ -1,3 +1,11 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+class BattleshipBoard:
+    """
+    A class that represents the board for the game Battleship.
+    """
+
+    def __init__(self, board):
+        """
+        The constructor for BattleshipBoard class.
+        """
+
+        self.board = board
