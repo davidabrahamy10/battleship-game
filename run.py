c91dfc5d9f09d23ff1af9b8e7a151ced4c623167
David Abraham <davidabrahamy10@gmail.com>
2023-02-02 11:18:04 +0000

    Add method to convert letters to numbers

--- run.py
+++ run.py
@@ -9,3 +9,9 @@ class BattleshipBoard:
         """
 
         self.board = board
+
+    def letters_and_numbers_dict(self):
+        """
+        A method that returns a dictionary mapping letters to numbers for use in printing the board.
+        """
+        return {"A": 0, "B": 1, "C": 2, "D": 3, "E": 4, "F": 5}
